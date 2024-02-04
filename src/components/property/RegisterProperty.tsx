c20e4b0ccb9f6b03d7c76837ca8bafb9d1b2c0fe
Robson Arita <robsonkeniti@gmail.com>
2024-02-04 18:03:41 -0300

    pretty form

--- src/components/property/RegisterProperty.tsx
+++ src/components/property/RegisterProperty.tsx
@@ -4,13 +4,14 @@ import { Button, Upload } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
 import { SelectorState } from '../../redux/reducers/authReducer';
+import './RegisterProperty.css'
 
 interface File {
   uid: string
-  _id: string
+  _id?: string
 }
 
-function removerPalavraEFormatar(stringOriginal: string, palavraARemover: string) {
+function removerPalavraEFormatar(stringOriginal: string, palavraARemover?: string) {
   // Dividir a string original usando a vírgula como delimitador
   let palavras = stringOriginal.split(',');
 
@@ -27,6 +28,7 @@ const PropertyForm = () => {
   const [propertyInfo, setPropertyInfo] = useState({
     title: '',
     description: '',
+    address: '',
     image: '',
   });
 
@@ -40,20 +42,20 @@ const PropertyForm = () => {
     }));
   };
 
-  const handleImageChange = (info: { file: { status: string, uid: string, response: { _id: string; }; }; }) => {
+  const handleImageChange = (info: { file: { status?: string, uid: string, response?: { _id: string; }; }; }) => {
     console.log({ info })
     if (info.file.status === 'done') {
       console.log({ response: info.file.response })
 
       const sentFiles = files
-      sentFiles.push({ uid: info.file.uid, _id: info.file.response._id })
+      sentFiles.push({ uid: info.file.uid, _id: info.file?.response?._id })
       setFiles(sentFiles)
       console.log({ files })
 
       // Atualiza o estado com a URL da imagem após o upload
       setPropertyInfo({
         ...propertyInfo,
-        image: propertyInfo.image?.length ? propertyInfo.image + ',' + info.file.response._id : info.file.response._id,
+        image: propertyInfo.image?.length ? propertyInfo.image + ',' + info.file.response?._id : info.file.response?._id ?? '',
       })
     }
 
@@ -62,7 +64,7 @@ const PropertyForm = () => {
       console.log({ removedFile })
       if (removedFile) {
         const filesFromPropertyInfo = propertyInfo.image
-        const updated = removerPalavraEFormatar(filesFromPropertyInfo, removedFile._id)
+        const updated = removerPalavraEFormatar(filesFromPropertyInfo, removedFile?._id)
         setPropertyInfo({
           ...propertyInfo,
           image: updated
@@ -77,6 +79,7 @@ const PropertyForm = () => {
     const formData = new FormData()
     formData.append('title', propertyInfo.title)
     formData.append('description', propertyInfo.description)
+    formData.append('address', propertyInfo.address)
     formData.append('image', propertyInfo.image)
 
     try {
@@ -93,36 +96,42 @@ const PropertyForm = () => {
 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Título:
-        <input type="text" name="title" value={propertyInfo.title} onChange={handleInputChange} />
-      </label>
+    <div className='divForm'>
+          <form onSubmit={handleSubmit} className="form">
+      <div className="form-group">
+        <label htmlFor="title">Título:</label>
+        <input type="text" id="title" name="title" value={propertyInfo.title} onChange={handleInputChange} />
+      </div>
 
-      <label>
-        Descrição:
-        <textarea name="description" value={propertyInfo.description} onChange={handleInputChange} />
-      </label>
+      <div className="form-group">
+        <label htmlFor="address">Endereço:</label>
+        <input type="text" id="address" name="address" value={propertyInfo.address} onChange={handleInputChange} />
+      </div>
 
-      <label>
-        Imagem:
+      <div className="form-group">
+        <label htmlFor="description">Descrição:</label>
+        <textarea id="description" name="description" value={propertyInfo.description} onChange={handleInputChange} />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="image">Imagens:</label>
         <Upload
-          action={process.env.REACT_APP_BASE_URL + '/auth/file/upload'} // rota back para upload
+          action={process.env.REACT_APP_BASE_URL + '/auth/file/upload'}
           method="post"
           headers={{
             Authorization: getToken(),
           }}
           listType="picture"
-          // @ts-ignore
-          onChange={handleImageChange} // Adiciona a função para lidar com as mudanças de upload
+          onChange={handleImageChange}
         >
           <Button icon={<UploadOutlined />}>Upload</Button>
         </Upload>
-      </label>
+      </div>
 
       <button type="submit">Cadastrar Imóvel</button>
     </form>
-  );
+    </div>
+  )
 };
 
 export default PropertyForm;
